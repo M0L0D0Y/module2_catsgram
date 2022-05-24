b3ac59cdf68871573b12c6dad430b72d9dba51ca
M0L0D0Y <i7ascha@gmail.com>
2022-05-24 19:02:43 +0300

    Добавил эндпоинты для пост и юзер(для юзер не работают)

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class SimpleController {
 
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
     @GetMapping
     public String homePage() {
+        log.info("Запрос получен.");
         return "Котограм";
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,38 +1,47 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
 import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
 
+import java.util.Collection;
 import java.util.HashMap;
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final HashMap<String, User> users = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
-    public HashMap<String, User> findAllUsers() {
-        return users;
+    public Collection<User> findAllUsers() {
+        log.debug("Текущее количество пользователей: {}", users.size());
+        return users.values();
     }
 
     @PostMapping
-    public void createUser(@RequestBody User user) throws InvalidEmailException, UserAlreadyExistException {
+    public User createUser(@RequestBody User user) throws InvalidEmailException, UserAlreadyExistException {
         if ((user.getEmail() == null) && (user.getEmail().equals(""))) {
             throw new InvalidEmailException("Отсутствует email");
         }
         if (users.containsKey(user.getEmail())) {
             throw new UserAlreadyExistException("Пользователь с таким email" + user.getEmail() + " уже существует");
         }
+        log.debug("Создан пользователь: {}", user);
         users.put(user.getEmail(), user);
+        return user;
     }
 
     @PutMapping
-    public void updateUser(@RequestBody User user) throws InvalidEmailException {
+    public User updateUser(@RequestBody User user) throws InvalidEmailException {
         if ((user.getEmail() == null) && (user.getEmail().equals(""))) {
             throw new InvalidEmailException("Отсутствует email");
         }
+        log.debug("Обновлен/создан пользователь: {}", user);
         users.put(user.getEmail(), user);
+        return user;
     }
 }

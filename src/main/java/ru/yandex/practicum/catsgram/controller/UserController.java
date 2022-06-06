b1dcab4428b76fa5622e1e0b69622353847344f8
M0L0D0Y <i7ascha@gmail.com>
2022-06-06 13:35:17 +0300

    refactor: разделил обработку запросов и бизнес-логику класса UserController

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -1,47 +1,37 @@
 package ru.yandex.practicum.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
 import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.util.Collection;
-import java.util.HashMap;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private final HashMap<String, User> users = new HashMap<>();
-    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findAllUsers() {
-        log.debug("Текущее количество пользователей: {}", users.size());
-        return users.values();
+        return userService.findAllUsers();
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) throws InvalidEmailException, UserAlreadyExistException {
-        if ((user.getEmail() == null) && (user.getEmail().equals(""))) {
-            throw new InvalidEmailException("Отсутствует email");
-        }
-        if (users.containsKey(user.getEmail())) {
-            throw new UserAlreadyExistException("Пользователь с таким email" + user.getEmail() + " уже существует");
-        }
-        log.debug("Создан пользователь: {}", user);
-        users.put(user.getEmail(), user);
-        return user;
+        return userService.createUser(user);
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) throws InvalidEmailException {
-        if ((user.getEmail() == null) && (user.getEmail().equals(""))) {
-            throw new InvalidEmailException("Отсутствует email");
-        }
-        log.debug("Обновлен/создан пользователь: {}", user);
-        users.put(user.getEmail(), user);
-        return user;
+        return userService.updateUser(user);
     }
 }
---
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -0,0 +1,44 @@
+package ru.yandex.practicum.catsgram.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.controller.UserController;
+import ru.yandex.practicum.catsgram.exception.InvalidEmailException;
+import ru.yandex.practicum.catsgram.exception.UserAlreadyExistException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@Service
+public class UserService {
+    private final HashMap<String, User> users = new HashMap<>();
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    public Collection<User> findAllUsers() {
+        log.debug("Текущее количество пользователей: {}", users.size());
+        return users.values();
+    }
+
+    public User createUser(User user) throws InvalidEmailException, UserAlreadyExistException {
+        if ((user.getEmail() == null) && (user.getEmail().equals(""))) {
+            throw new InvalidEmailException("Отсутствует email");
+        }
+        if (users.containsKey(user.getEmail())) {
+            throw new UserAlreadyExistException("Пользователь с таким email" + user.getEmail() + " уже существует");
+        }
+        log.debug("Создан пользователь: {}", user);
+        users.put(user.getEmail(), user);
+        return user;
+    }
+
+    public User updateUser(User user) throws InvalidEmailException {
+        if ((user.getEmail() == null) && (user.getEmail().equals(""))) {
+            throw new InvalidEmailException("Отсутствует email");
+        }
+        log.debug("Обновлен/создан пользователь: {}", user);
+        users.put(user.getEmail(), user);
+        return user;
+    }
+}

dd92b6a30baaba95cfdb0964e6b731d615f29b55
M0L0D0Y <98527122+M0L0D0Y@users.noreply.github.com>
2022-06-08 13:45:33 +0300

    feat: реализовал постраничный просмотр ленты постов

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -18,15 +18,28 @@ public class PostController {
         this.postService = postService;
     }
 
-    @GetMapping()
-    public List<Post> findAll() {
-        return postService.findAll();
+    @GetMapping
+    public List<Post> findAll(
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort) {
+
+        if (!(sort.equals("asc") || sort.equals("desc"))) {
+            throw new IllegalArgumentException();
+        }
+        if (page < 0 || size <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Integer from = page * size;
+        return postService.findAll(size, from, sort);
     }
 
     @PostMapping()
     public Post create(@RequestBody Post post) {
         return postService.create(post);
     }
+
     @GetMapping("/{postId}")
     public Optional<Post> findById(@PathVariable int postId) {
         return postService.findById(postId);
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -7,32 +7,47 @@ import ru.yandex.practicum.catsgram.model.Post;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
     private final List<Post> posts = new ArrayList<>();
     private final UserService userService;
+    private static Integer globalId = 0;
+
 
     public PostService(UserService userService) {
         this.userService = userService;
     }
 
 
-    public List<Post> findAll() {
-        return posts;
+    public List<Post> findAll(Integer size, Integer from, String sort) {
+        return posts.stream().sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate());
+            if (sort.equals("desc")) {
+                comp = -1 * comp;
+            }
+            return comp;
+        }).skip(from).limit(size).collect(Collectors.toList());
     }
+
 
     public Post create(Post post) {
         if (userService.findUserByEmail(post.getAuthor()) == null) {
             throw new UserNotFoundException("Пользователь " + post.getAuthor() + "не найден");
         }
+        post.setId(getNextId());
         posts.add(post);
         return post;
     }
 
     public Optional<Post> findById(int postId) {
-            return posts.stream()
-                    .filter(x -> x.getId() == postId)
-                    .findFirst();
+        return posts.stream()
+                .filter(x -> x.getId() == postId)
+                .findFirst();
+    }
+
+    private static Integer getNextId() {
+        return globalId++;
     }
 }
